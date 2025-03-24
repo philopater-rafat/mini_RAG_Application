aee8e797f094d322aa8672208845eaa6b26576e8
philopater <philorafat2002@gmail.com>
2025-03-24 20:58:38 +0200

    add another welcome request

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ def welcome():
     return {
         "message": "Welcome to FastAPI"
         }
+
+@app.get("/welcome1")
+def welcome():
+    return {
+        "message1": "Welcome to FastAPI 1"
+        }
