05b6f68c5be67a69d4153a405ccff310899e21ec
philopater <philorafat2002@gmail.com>
2025-03-29 04:06:44 +0200

    including data router

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI # type: ignore
 from dotenv import load_dotenv # type: ignore
-load_dotenv('.env')
-
-from routes import base
+load_dotenv('.env') # Load environment variables from .env file
+from routes import base, data
 
 app = FastAPI()
 
 app.include_router(base.base_router)
+app.include_router(data.data_router)
